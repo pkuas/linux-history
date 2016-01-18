dc8ee6c69317b5f44dccadc16c0f81cde79c0e14
CQY-asus-win10 <qychen@pku.edu.cn>
2016-01-18 13:10:36 +0800

    add some notes about structrue ana

--- analysis.R
+++ analysis.R
@@ -13,7 +13,7 @@ dels.month.arr <- tapply(delta$del, delta$m, sum)
 png("loc-month.png", width=800,height=600);
 plot(as.numeric(names(adds.month.arr)), adds.month.arr, ylim=c(1,1e6),main = "Lines of code each month",type="l",xlab="Tenure (months)",ylab="# each month");
 lines(as.numeric(names(dels.month.arr)),dels.month.arr, col=2);
-legend(2005, 1e6,legend=c("Lines of added code","Lines of deleted code"),cex=1,lwd=2,col=rep(1:2),bg="white"); 
+legend(2005, 1e6,legend=c("Lines of added code","Lines of deleted code"),cex=1,lwd=2,col=rep(1:2),bg="white");
 dev.off();
 
 ## how num of changes, files/modules touched varys
@@ -25,7 +25,7 @@ png("changes.files.mods-month.png", width=800,height=600);
 plot(as.numeric(names(chgs.month.arr)), chgs.month.arr, ylim=c(1,tt+10),main = "# of changes, files and modules touched each month",type="l",xlab="Tenure (months)",ylab="# each month", col=1);
 lines(as.numeric(names(files.month.arr)),files.month.arr, col=2);
 lines(as.numeric(names(mods.month.arr)),mods.month.arr * 100, col=3);
-legend(2010, tt,legend=c("# of changes","# of files touched", "# of modules touched * 100"),cex=1,lwd=2,col=rep(1:3),bg="white"); 
+legend(2010, tt,legend=c("# of changes","# of files touched", "# of modules touched * 100"),cex=1,lwd=2,col=rep(1:3),bg="white");
 dev.off()
 
 ## how num of authors/committers/joiners/LTCs/core-committers varys
@@ -43,7 +43,7 @@ plot(as.numeric(names(authors.month.arr)), authors.month.arr, ylim=c(1,tt+10),ma
 lines(as.numeric(names(cmtrs.month.arr)),cmtrs.month.arr, col=2);
 lines(as.numeric(names(joiners.month.arr)),joiners.month.arr, col=4);
 lines(as.numeric(names(ltcs.month.arr)),ltcs.month.arr, col=3);
-legend(2005, tt,legend=c("# of authors","# of committers", "# of joiners", "# of LTCs"),cex=1,lwd=2,col=rep(1:4),bg="white"); 
+legend(2005, tt,legend=c("# of authors","# of committers", "# of joiners", "# of LTCs"),cex=1,lwd=2,col=rep(1:4),bg="white");
 dev.off()
 png("ltcratio-month.png", width=800,height=600)
 tt <- max(ltcratio.month.arr)
@@ -60,7 +60,7 @@ png("changes.files.mods-avrgpson.month.png", width=800,height=600);
 plot(as.numeric(names(chgspson.month.arr)), chgspson.month.arr, ylim=c(1,tt),main = "Average productivity over time",type="l",xlab="Tenure (months)",ylab="#", col=1);
 lines(as.numeric(names(flspson.month.arr)), flspson.month.arr * 2, col=2);
 lines(as.numeric(names(modspson.month.arr)), modspson.month.arr * 10, col=3);
-legend(2005, tt,legend=c("# of changes per author","# of files per author * 2", "# of modules per author * 10"),cex=1,lwd=2,col=rep(1:3),bg="white"); 
+legend(2005, tt,legend=c("# of changes per author","# of files per author * 2", "# of modules per author * 10"),cex=1,lwd=2,col=rep(1:3),bg="white");
 dev.off()
 
 ## how avg of LOC per person varys
@@ -70,7 +70,7 @@ tt <- 80
 png("adds.dels-avrgpchgpson.month.png", width=800,height=600);
 plot(as.numeric(names(addspson.month.arr)), addspson.month.arr, ylim=c(0,tt),main = "Average productivity(LOC) over time",type="l",xlab="Tenure (months)",ylab="#", col=1);
 lines(as.numeric(names(delspson.month.arr)), delspson.month.arr, col=2);
-legend(2005, tt,legend=c("LOC added per change per author","LOC deleted per change per author"),cex=1,lwd=2,col=rep(1:2),bg="white"); 
+legend(2005, tt,legend=c("LOC added per change per author","LOC deleted per change per author"),cex=1,lwd=2,col=rep(1:2),bg="white");
 dev.off()
 
 # structure analysis
@@ -81,27 +81,27 @@ authors.numMods.tb <- table(mods.author.arr)
 mods.cmtr.arr <- tapply(delta$mod, delta$cid, numOfUnique)
 cmtrs.numMods.tb <- table(mods.cmtr.arr)
 png("CFG.dvpr-numMods.png", width=800,height=600);
-plot(c(0, as.numeric(names(authors.numMods.tb))), 
-	c(0, cumsum(authors.numMods.tb/sum(authors.numMods.tb))), 
+plot(c(0, as.numeric(names(authors.numMods.tb))),
+	c(0, cumsum(authors.numMods.tb/sum(authors.numMods.tb))),
 	main = "CFG of developers' modules",xlab="Number of modules",ylab="CF",
 	ylim=c(0,1), pch=2, lty=3, type='b', col=2);
 lines(c(0, as.numeric(names(cmtrs.numMods.tb))), c(0, cumsum(cmtrs.numMods.tb/sum(cmtrs.numMods.tb))),type='b', pch=2, lty=3, col=3);
 legend(10, 0.6,legend=paste(c("author: total","committer: total"),
 	c(sum(authors.numMods.tb), sum(cmtrs.numMods.tb))),
-	cex=1,lwd=2,col=2:3,bg="white"); 
+	cex=1,lwd=2,col=2:3,bg="white");
 dev.off()
 ## adjusted num of modules
 adjmods.author.arr <- tapply(delta$mod, delta$aid, adjNumOfUnique)
 adjmods.cmtr.arr <- tapply(delta$mod, delta$cid, adjNumOfUnique)
 png("box.dvprs-adjNumMods.png", width=800,height=600);
-boxplot(numMods ~ dvpr, 
-	data=data.frame(numMods=c(adjmods.author.arr, adjmods.cmtr.arr), 
-		dvpr=c(rep('author', length(adjmods.author.arr)), 
+boxplot(numMods ~ dvpr,
+	data=data.frame(numMods=c(adjmods.author.arr, adjmods.cmtr.arr),
+		dvpr=c(rep('author', length(adjmods.author.arr)),
 			rep('committer', length(adjmods.cmtr.arr)))),
 	main="Developers' adjusted modules", ylab='# of adjusted modules')
-text(2,c(4.5, 4.3), labels=paste(c("# of authors:","# of committers:"), 
+text(2,c(4.5, 4.3), labels=paste(c("# of authors:","# of committers:"),
 	c(length(adjmods.author.arr), length(adjmods.cmtr.arr))),
-	cex=1,col=1,bg="white"); 
+	cex=1,col=1,bg="white");
 dev.off()
 
 
@@ -115,21 +115,21 @@ cmtrs.numMods.indrivers.tb <- table(mods.cmtr.indrivers.arr)
 png("CFG.dvprs-numMods.indrivers.png", width=800,height=600);
 plot(c(0, as.numeric(names(authors.numMods.indrivers.tb))), c(0, cumsum(authors.numMods.indrivers.tb/sum(authors.numMods.indrivers.tb))), ylim=c(0,1),main = "CFG of developers' modules in drivers",type='b', pch=2, lty=3,xlab="Number of modules",ylab="CF", col=2);
 lines(c(0, as.numeric(names(cmtrs.numMods.indrivers.tb))), c(0, cumsum(cmtrs.numMods.indrivers.tb/sum(cmtrs.numMods.indrivers.tb))), type='b', pch=2, lty=3, col=3);
-legend(50, 0.6,legend=c("author","committer"),cex=1,lwd=2,col=2:3,bg="white"); 
+legend(50, 0.6,legend=c("author","committer"),cex=1,lwd=2,col=2:3,bg="white");
 dev.off()
 ### adjusted
 adjmods.author.indrivers.arr <- tapply(delta$mmod[drivers.delta.sel], delta$aid[drivers.delta.sel], adjNumOfUnique)
 adjmods.cmtr.indrivers.arr <- tapply(delta$mmod[drivers.delta.sel], delta$cid[drivers.delta.sel], adjNumOfUnique)
 png("box.dvprs-adjNumMods.indrivers.png", width=800,height=600);
 boxplot(numMods ~ dvpr,
-	data=data.frame(numMods=c(adjmods.author.indrivers.arr, adjmods.cmtr.indrivers.arr), 
-		dvpr=c(rep('author', length(adjmods.author.indrivers.arr)), 
+	data=data.frame(numMods=c(adjmods.author.indrivers.arr, adjmods.cmtr.indrivers.arr),
+		dvpr=c(rep('author', length(adjmods.author.indrivers.arr)),
 			rep('committer', length(adjmods.cmtr.indrivers.arr)))),
 	main="Developers' adjusted modules in drivers", ylab='# of adjusted modules')
 axis(2, c(1, seq(2, max(adjmods.author.indrivers.arr) + 1, 2)))
-text(1.5,c(9.5, 9.3), labels=paste(c("# of authors:","# of committers:"), 
+text(1.5,c(9.5, 9.3), labels=paste(c("# of authors:","# of committers:"),
 	c(length(adjmods.author.indrivers.arr), length(adjmods.cmtr.indrivers.arr))),
-	cex=1,col=1,bg="white"); 
+	cex=1,col=1,bg="white");
 dev.off()
 
 ## in drivers/staging
@@ -143,25 +143,25 @@ png("CFG.dvprs-numMods.instaging.png", width=800,height=600);
 plot(c(0, as.numeric(names(authors.numMods.instaging.tb))), c(0, cumsum(authors.numMods.instaging.tb/sum(authors.numMods.instaging.tb))), ylim=c(0,1),main = "CFG of developers' modules in staging",type='b', pch=2, lty=3,xlab="Number of modules",ylab="CF", col=2);
 lines(c(0, as.numeric(names(cmtrs.numMods.instaging.tb))), c(0, cumsum(cmtrs.numMods.instaging.tb/sum(cmtrs.numMods.instaging.tb))), type='b', pch=2, lty=3, col=3);
 abline(h=c(0.8, 0.9, 0.95), lty=2, col='gray')
-legend(50, 0.6,legend=c("author","committer"),cex=1,lwd=2,col=2:3,bg="white"); 
+legend(50, 0.6,legend=c("author","committer"),cex=1,lwd=2,col=2:3,bg="white");
 dev.off()
 ### adjusted
 adjmods.author.instaging.arr <- tapply(delta$smod[staging.delta.sel], delta$aid[staging.delta.sel], adjNumOfUnique)
 adjmods.cmtr.instaging.arr <- tapply(delta$smod[staging.delta.sel], delta$cid[staging.delta.sel], adjNumOfUnique)
 png("box.dvprs-adjNumMods.instaging.png", width=800,height=600);
 boxplot(numMods ~ dvpr,
-	data=data.frame(numMods=c(adjmods.author.instaging.arr, adjmods.cmtr.instaging.arr), 
-		dvpr=c(rep('author', length(adjmods.author.instaging.arr)), 
+	data=data.frame(numMods=c(adjmods.author.instaging.arr, adjmods.cmtr.instaging.arr),
+		dvpr=c(rep('author', length(adjmods.author.instaging.arr)),
 			rep('committer', length(adjmods.cmtr.instaging.arr)))),
 	main="Developers' adjusted modules in staging", ylab='# of adjusted modules')
 axis(2, c(1, seq(2, max(adjmods.author.instaging.arr) + 1, 2)))
-text(1.5,c(9.5, 8.3), labels=paste(c("# of authors:","# of committers:"), 
+text(1.5,c(9.5, 8.3), labels=paste(c("# of authors:","# of committers:"),
 	c(length(adjmods.author.instaging.arr), length(adjmods.cmtr.instaging.arr))),
-	cex=1,col=1,bg="white"); 
+	cex=1,col=1,bg="white");
 dev.off()
 
-## 
-### It could be this case: core authors/committers touched more modules, so we need 
+##
+### It could be this case: core authors/committers touched more modules, so we need
 ### core authors
 chgsofauthor.utb <- table(delta$aid)
 chgsofauthor.tb <- sort(chgsofauthor.utb, decreasing=T)
@@ -182,7 +182,7 @@ mods.coreCmtr.arr <- tapply(delta$mod[coreCmtr.delta.sel], delta$cid[coreCmtr.de
 #### CFG
 coreCmtrs.numMods.tb <- table(mods.coreCmtr.arr)
 png("CFG.coreDvprs-numMods.png", width=800,height=600);
-plot(c(0, as.numeric(names(coreAuthors.numMods.tb))), 
+plot(c(0, as.numeric(names(coreAuthors.numMods.tb))),
 	c(0, cumsum(coreAuthors.numMods.tb)/sum(coreAuthors.numMods.tb)),
 	main = "CFG of core developers' modules", xlab="Number of modules", ylab="CF",
 	ylim = c(0, 1),	type='b',pch=2, lty=3, col=2)
@@ -190,15 +190,15 @@ lines(c(0, as.numeric(names(coreCmtrs.numMods.tb))),
 	c(0, cumsum(coreCmtrs.numMods.tb)/sum(coreCmtrs.numMods.tb)),
 	type='b',pch=2, lty=3, col=3)
 abline(h=c(0.8, 0.9, 0.95), lty=2, col='gray')
-legend(10, 0.6, legend=paste(c("author: total","committer: total"), 
+legend(10, 0.6, legend=paste(c("author: total","committer: total"),
 	c(sum(coreAuthors.numMods.tb), sum(coreCmtrs.numMods.tb))),
-	cex=1,lwd=2,col=2:3,bg="white"); 
+	cex=1,lwd=2,col=2:3,bg="white");
 dev.off()
 #### boxplot
 png("box.coreDvprs-numMods.png", width=800,height=600);
-boxplot(numMods ~ dvpr, 
-	data=data.frame(numMods=c(mods.coreAuthor.arr, mods.coreCmtr.arr), 
-		dvpr=c(rep('author', length(mods.coreAuthor.arr)), 
+boxplot(numMods ~ dvpr,
+	data=data.frame(numMods=c(mods.coreAuthor.arr, mods.coreCmtr.arr),
+		dvpr=c(rep('author', length(mods.coreAuthor.arr)),
 			rep('committer', length(mods.coreCmtr.arr)))),
 	main="Core developers' modules", ylab='# of modules')
 dev.off()
@@ -208,17 +208,81 @@ dev.off()
 adjmods.coreAuthor.arr <- tapply(delta$mod[coreAuthor.delta.sel], delta$aid[coreAuthor.delta.sel], adjNumOfUnique)
 adjmods.coreCmtr.arr <- tapply(delta$mod[coreCmtr.delta.sel], delta$cid[coreCmtr.delta.sel], adjNumOfUnique)
 png("box.coreDvprs-adjNumMods.png", width=800,height=600);
-boxplot(numMods ~ dvpr, 
-	data=data.frame(numMods=c(adjmods.coreAuthor.arr, adjmods.coreCmtr.arr), 
-		dvpr=c(rep('author', length(adjmods.coreAuthor.arr)), 
+boxplot(numMods ~ dvpr,
+	data=data.frame(numMods=c(adjmods.coreAuthor.arr, adjmods.coreCmtr.arr),
+		dvpr=c(rep('author', length(adjmods.coreAuthor.arr)),
 			rep('committer', length(adjmods.coreCmtr.arr)))),
 	main="Core developers' adjusted modules", ylab='# of adjusted modules')
-text(2,c(3.5, 3.3), labels=paste(c("# of authors:","# of committers:"), 
+text(2,c(3.5, 3.3), labels=paste(c("# of authors:","# of committers:"),
 	c(length(adjmods.coreAuthor.arr), length(adjmods.coreCmtr.arr))),
-	cex=1,col=1,bg="white"); 
+	cex=1,col=1,bg="white");
 dev.off()
 
 # Membership
 mods.author.utb <- table(delta$aid, delta$mod)
 mods.author.uptb <- prop.table(mods.author.utb, 1)
 mods.coreAuthor.uptb <- mods.author.uptb[coreAuthor.chgsofauthor.sel[rownames(mods.author.uptb)], ]
+
+# author-committer mapping
+numAthrs.cmtr.arr <- tapply(delta$aid, delta$cid, numOfUnique)
+adjNumAthrs.cmtr.arr <- tapply(delta$aid, delta$cid, adjNumOfUnique)
+numCmtrs.athr.arr <- tapply(delta$cid, delta$aid, numOfUnique)
+adjNumCmtrs.athr.arr <- tapply(delta$cid, delta$aid, adjNumOfUnique)
+
+numAthrs.cmtr.indrivers.arr <- tapply(delta$aid[drivers.delta.sel], delta$cid[drivers.delta.sel], numOfUnique)
+adjNumAthrs.cmtr.indrivers.arr <- tapply(delta$aid[drivers.delta.sel], delta$cid[drivers.delta.sel], adjNumOfUnique)
+
+getNumVar2EachVar1InPrdofDelta<-function(sel, prd, var1, var2, measure) {
+	return(tapply((1:numofdeltas)[sel], delta[sel, prd],
+		function(x) {return(tapply(delta[x, var2], delta[x, var1], measure))}))
+}
+convtArrOfListToDF<-function(arrList) {
+	t <- data.frame(cid=character(0),m=character(0),adjNumAthrs=numeric(0))
+	for (m in names(arrList)){
+		tv <- arrList[[m]]
+		t <- rbind(t, data.frame(cid=names(tv),m=rep(m, length(tv)),
+			adjNumAthrs=tv, row.names=NULL))
+	}
+	return(t)
+}
+cmtryear <- data.frame(cid=character(0),m=character(0),adjNumAthrs=numeric(0),mod=character(0))
+t <- convtArrOfListToDF(getNumVar2EachVar1InPrdofDelta(drivers.delta.sel, 'y', 'cid', 'aid', adjNumOfUnique))
+t$mod <- 'drivers'
+cmtryear <- rbind(cmtryear, t)
+t <- convtArrOfListToDF(getNumVar2EachVar1InPrdofDelta(!drivers.delta.sel, 'y', 'cid', 'aid', adjNumOfUnique))
+t$mod <- 'ndrivers'
+cmtryear <- rbind(cmtryear, t)
+cmtryear$mod.f<-factor(cmtryear$mod,
+    levels=c('drivers', 'ndrivers'), labels=c('drivers', 'ndrivers'))
+tmod <- c('ndrivers', 'drivers')[drivers.delta.sel + 1]
+t <- as.data.frame(table(delta$cid, delta$y, tmod))
+colnames(t) <- c('cid', 'm', 'mod', 'numChg')
+cmtryear <- merge(cmtryear, t, by=c('cid', 'm', 'mod'))
+tdf <- data.frame(cid=character(0),m=character(0),adjNumAthrs=numeric(0),mod=character(0))
+t <- convtArrOfListToDF(getNumVar2EachVar1InPrdofDelta(drivers.delta.sel, 'y', 'cid', 'aid', numOfUnique))
+t$mod <- 'drivers'
+tdf <- rbind(tdf, t)
+t <- convtArrOfListToDF(getNumVar2EachVar1InPrdofDelta(!drivers.delta.sel, 'y', 'cid', 'aid', numOfUnique))
+t$mod <- 'ndrivers'
+tdf <- rbind(tdf, t)
+colnames(tdf) <- c('cid', 'm', 'numAthrs','mod')
+cmtryear <- merge(cmtryear, tdf, by=c('cid', 'm', 'mod'))
+
+png("box.numChg-cmtr.year.png", width=800,height=600);
+boxplot(numChg ~ mod * m, data=cmtryear, col=c("gold", 'darkgreen'))
+title('Boxplot of # of changes for committers each year')
+dev.off()
+
+stats::cor(cmtryear[,c('adjNumAthrs', 'numChg')], method='pearson')
+stats::cor(cmtryear[,c('adjNumAthrs', 'numChg')], method='spearman')
+stats::cor(cmtryear[cmtryear$mod=='drivers',c('adjNumAthrs', 'numChg')], method='pearson')
+stats::cor(cmtryear[cmtryear$mod=='drivers',c('adjNumAthrs', 'numChg')], method='spearman')
+stats::cor(cmtryear[cmtryear$mod!='drivers',c('adjNumAthrs', 'numChg')], method='pearson')
+stats::cor(cmtryear[cmtryear$mod!='drivers',c('adjNumAthrs', 'numChg')], method='spearman')
+
+
+
+png("box.cmtryear.png", width=800,height=600);
+b<-boxplot(adjNumAthrs ~ mod.f *m, data=cmtryear, ylim=c(0, 8), col=c("gold","darkgreen"))
+axis(2, c(1, 8, 2))
+dev.off()
--- note.md
+++ note.md
@@ -27,9 +27,38 @@ See below
 * Documentation/SubmitChecklist
 
 # Merge Aliases
-一个开发者可能有多个name、email，使用简单的并查集算法得到每个开发者使用多少个alias。下表的第一行表示alias数量，第二行表示使用这么多数量alias的开发者数量。
+一个开发者可能有多个name、email，使用简单的并查集算法得到每个开发者使用多少个alias。下表的第一行表示alias数量，第二行表示使用这么多数量alias的开发者数量，第三行表示占比。
+```
+      [,1]    [,2]    [,3]   [,4]   [,5]  [,6] [,7]  [,8]  [,9] [,10] [,11]
+[1,]  1.00    2.00    3.00   4.00   5.00  6.00  7.0  8.00  9.00 10.00 11.00
+[2,] 91.00 9220.00 1804.00 639.00 256.00 93.00 49.0 29.00 18.00  9.00  5.00
+[3,]  0.74   75.41   14.75   5.23   2.09  0.76  0.4  0.24  0.15  0.07  0.04
+     [,12] [,13] [,14] [,15] [,16] [,17]
+[1,] 12.00 13.00 14.00 15.00 17.00 29.00
+[2,]  4.00  1.00  4.00  1.00  3.00  1.00
+[3,]  0.03  0.01  0.03  0.01  0.02  0.01
+```
 
-|# aliases|1|2|3|4|5|6|7|8|9|10|11|12|13|14|15|17|29|
-|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|
-|# dvprs|91|9220|1804|639|256|93|49|29|18|9|5|4|1|4|1|3|1|
-|%|0.74|75.41|14.75|5.23|2.09|0.76|0.40|0.24|0.15|0.07|0.04|0.03|0.01|0.03|0.01|0.02|0.01|
+# Core Author/Committer Definition
+如果按照贡献delta数量来定义，即把delta数量从大到小排序，取累计80%delta对应的作者为core Author/Committer，可能会有这样的问题：一个模块（如drivers）集中了大量的delta，那么这种定义方式就会把其他模块的Author/Committer过滤掉。
+
+还是先搞清楚structure
+
+# structure
+要分析structure，可以分析
+- 每个author/committer为几个模块做贡献，如何分布
+- author/committer如何对应起来，每个author的代码由几个committer提交，每个committer提交几个人的代码，这些量是如何分布的
+
+![x](pics/CFG.dvpr-numMods.png)
+![x](pics/CFG.dvprs-numMods.indrivers.png)
+![x](pics/CFG.dvprs-numMods.instaging.png)
+![x](pics/CFG.coreDvprs-numMods.png)
+
+![开发者相关模块数量](./pics/box.dvprs-adjNumMods.png)
+![x](./pics/box.dvprs-adjNumMods.indrivers.png)
+![x](./pics/box.dvprs-adjNumMods.instaging.png)
+
+
+### 所以，我们看到了两个问题
+- 只要开发者和某个模块有一次接触，就为这个开发者的模块数加一，这是不合理的。一种修正方法是：对一个开发者而言，确定其贡献最多的模块（根据delta数量,maxdelta），为该开发者模块数加一，对于其他的模块，加上delta数量/maxdelta。
+- 把所有的author或者committer混为一谈，是不合适的。由于贡献多的开发者数量少，其涉及的模块较多，故而当把所有的author放一起画boxplot时，boxplot显示的“正常”数据点是贡献少的开发者，而贡献多得开发者成了outlier。
