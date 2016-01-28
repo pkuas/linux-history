677882e1ef91786d9bdf4faf0916fdeaa3c28aea
CQY-asus-win10 <qychen@pku.edu.cn>
2016-01-28 14:34:10 +0800

    athrTrcTree3year; cmtrTrcTree3year; but seems wrong

--- analysis.R
+++ analysis.R
@@ -827,6 +827,7 @@ zipTrc <- function(trc) {
 }
 athrTrcZip <- lapply(athrTrc, zipTrc)
 cmtrTrcZip <- lapply(cmtrTrc, zipTrc)
+## all years for author
 athrTrcTree <- list()
 res <- lapply(athrTrcZip, function(idx){
     pathes <- Reduce(paste, delta$mainMod[idx], accumulate=T)
@@ -848,6 +849,29 @@ sink('athrTrcTreeSmry.md', append=F)
 print(athrTrcTreeSmry)
 sink()
 
+## 3-year period for author
+athrTrcTree3year <- list()
+res <- lapply(athrTrcZip, function(idx) {
+	st <- delta$m[idx[1]] - 0.001
+	mth <- as.character(delta$m[idx[1]])
+	tsel <- delta$ty[idx] >= st & delta$ty[idx] < st + 3
+	idx <- idx[tsel]
+	pathes <- Reduce(paste, delta$mainMod[idx], accumulate=T)
+	nidx <- length(idx)
+    dtimes <- c(0, delta$ty[idx][-1] - delta$ty[idx][-nidx])
+    for (i in 1:min(nidx, 5)) {
+    	if (is.null(athrTrcTree3year[[pathes[i]]])) {athrTrcTree3year[[pathes[i]]] <<- list()}
+    }
+    if (is.null(athrTrcTree3year[[pathes[1]]][[mth]])) {athrTrcTree3year[[pathes[1]]][[mth]] <<- 1}
+    else {athrTrcTree3year[[pathes[1]]][[mth]] <<- athrTrcTree3year[[pathes[1]]][[mth]] + 1}
+    if (nidx == 1) return(NULL);
+    for (i in 2:min(nidx, 5)) {
+    	if (is.null(athrTrcTree3year[[pathes[i]]][[mth]])) {athrTrcTree3year[[pathes[i]]][[mth]] <<- c(dtimes[i])}
+    	else {athrTrcTree3year[[pathes[i]]][[mth]] <<- c(athrTrcTree3year[[pathes[i]]][[mth]], dtimes[i])}
+    }
+	})
+
+## all years for committer
 cmtrTrcTree <- list()
 res <- lapply(cmtrTrcZip, function(idx){
     pathes <- Reduce(paste, delta$mainMod[idx], accumulate=T)
@@ -868,3 +892,25 @@ cmtrTrcTreeSmry <- cmtrTrcTreeSmry[which(unlist(lapply(cmtrTrcTree, length)) >= 
 sink('cmtrTrcTreeSmry.md', append=F)
 print(cmtrTrcTreeSmry)
 sink()
+
+## 3-year period for committer
+cmtrTrcTree3year <- list()
+res <- lapply(cmtrTrcZip, function(idx) {
+	st <- delta$m[idx[1]] - 0.001
+	mth <- as.character(delta$m[idx[1]])
+	tsel <- delta$cty[idx] >= st & delta$cty[idx] < st + 3
+	idx <- idx[tsel]
+	pathes <- Reduce(paste, delta$mainMod[idx], accumulate=T)
+	nidx <- length(idx)
+    dtimes <- c(0, delta$cty[idx][-1] - delta$cty[idx][-nidx])
+    for (i in 1:min(nidx, 5)) {
+    	if (is.null(cmtrTrcTree3year[[pathes[i]]])) {cmtrTrcTree3year[[pathes[i]]] <<- list()}
+    }
+    if (is.null(cmtrTrcTree3year[[pathes[1]]][[mth]])) {cmtrTrcTree3year[[pathes[1]]][[mth]] <<- 1}
+    else {cmtrTrcTree3year[[pathes[1]]][[mth]] <<- cmtrTrcTree3year[[pathes[1]]][[mth]] + 1}
+    if (nidx == 1) return(NULL);
+    for (i in 2:min(nidx, 5)) {
+    	if (is.null(cmtrTrcTree3year[[pathes[i]]][[mth]])) {cmtrTrcTree3year[[pathes[i]]][[mth]] <<- c(dtimes[i])}
+    	else {cmtrTrcTree3year[[pathes[i]]][[mth]] <<- c(cmtrTrcTree3year[[pathes[i]]][[mth]], dtimes[i])}
+    }
+	})
